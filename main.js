47b2dd5f183f964e98233a032140547fb9683f0e
skwergles <maw.cade@yahoo.com>
2016-05-17 16:57:00 -0700

    created main js file and added variables

---
+++ main.js
@@ -0,0 +1,28 @@
+var canvas; //Will be linked to the canvas in our index.html page
+var stage; //Is the equivalent of stage in AS3; we'll add "children" to it
+ 
+// Graphics
+//[Background]
+ 
+var bg; //The background graphic
+ 
+//[Title View]
+  
+ 
+var main; //The Main Background
+var startB; //The Start button in the main menu
+var creditsB; //The credits button in the main menu
+ 
+//[Credits]
+ 
+ 
+var credits; //The Credits screen
+ 
+//[Game View]
+ 
+ 
+var player; //The player paddle graphic
+var ball; //The ball graphic
+var cpu; //The CPU paddle
+var win; //The winning popup
+var lose; //The losing popup
